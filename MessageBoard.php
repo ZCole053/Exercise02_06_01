3ffd243b26582b3ef67c6055430a15316043057a
Unknown <zcole053@west-mec.org>
2018-10-23 15:25:34 -0700

    Manipulating arrays
    We manipulated arrays and started working with associative arrays. There is some debugging that is needed on message board.

--- MessageBoard.php
+++ MessageBoard.php
@@ -11,21 +11,46 @@
 <h1>Message Board</h1>
 <?php
 if (isset($_GET['action'])){
-    if(file_exists("messages.txt") || filesize("messages.txt") != 0){
+    if(file_exists("messages.txt") || filesize("messages.txt") != 0){ 
         //opens the file
         $messageArray = file("messages.txt");
         switch($_GET['action']){
             case 'Delete First':
-            //function to delete the first thing in an array
+                //function to delete the first thing in an array
                 array_shift($messageArray);
                 break;
-                //checks to see if there is anything in the array
+            case 'Delete Last':
+                //function to delete the last thing in an array
+                array_pop($messageArray);
+                break;
+            case 'Delete Message':
+            if(isset($_GET['message'])){
+                array_splice($messageArray, $_GET['message'],1);
+            // $index = $_GET['message'];
+            // unset($messageArray[$index]);
+            // $messageArray = array_values($messageArray);
+            }
+                break;
+            case 'Remove Duplicates':
+                $messageArray = array_unique($messageArray);
+                $messageArray = array_values($messageArray);
+                break;
+        //checks to see if there is anything in the array
         }if(count($messageArray) > 0){
+            $newMessages = implode($messageArray);
+            $filehandle = fopen("messages.txt", "wb");
+            // if the file was written too yay if not
+            if(!$filehandle){
+                echo "There was an error updating the message file.\n";
+            }else{
+                fwrite($filehandle, $newMessages);
+                fclose($filehandle);
+            }
             echo "There are remaining messages in the array.<br>";//debug
         }else{
             //function to uncreate the file
             unlink("messages.txt");
-        }
+        } 
     }
 }
 //interperator will short circut
@@ -36,14 +61,26 @@ if(!file_exists("messages.txt") || filesize("messages.txt") == 0){
     $messageArray = file("messages.txt");
     echo "<table style=\"background-color:lightgrey\" border=\"1\" width=\"100%\">\n";
     $count = count($messageArray);
-    for ($i=0; $i < $count; $i++) { 
+    //associative array
+    for($i=0 ; $i < $count; $i++){
         $currMsg = explode("~",$messageArray[$i]);
+        $keMessageArray[$currMsg[0]] = $currMsg[1] . "~". $currMsg[2];
+
+    }
+    $index = 1;
+    $key = key($key);
+    foreach($keMessageArray as $message) { 
+        $currMsg = explode("~",$message);
         echo "<tr>\n";
-        echo "<td width =\"5%\" style=\"text-align:center; font-weight:bold\">". ($i + 1) . "</td>\n";
-        echo "<td width=\"95%\"><span style=\"font-weight: bold\">Subject: </span>" . htmlentities($currMsg[0]) . "<br>\n";
-        echo "<span style=\"font-weight: bold\">Name: </span>" . htmlentities($currMsg[1]) . "<br>\n";
-        echo "<span style=\"text-decoration: underline; font-weight: bold\">Message: </span><br>\n" . htmlentities($currMsg[2]) . "</td>\n";
+        echo "<td width =\"5%\" style=\"text-align:center; font-weight:bold\">". $index . "</td>\n";
+        echo "<td width=\"85%\"><span style=\"font-weight: bold\">Subject: </span>" . htmlentities($key) . "<br>\n";
+        echo "<span style=\"font-weight: bold\">Name: </span>" . htmlentities($currMsg[0]) . "<br>\n";
+        echo "<span style=\"text-decoration: underline; font-weight: bold\">Message: </span><br>\n" . htmlentities($currMsg[1]) . "</td>\n";
+        echo "<td width=\"10%\" style=\"text-align:center\">". "<a href='MessageBoard.php?". "action=Delete%20Message&". "message=". ($index-1). "'>" . "Delete this Message</a></td>";
         echo "</tr>\n";
+        ++$index;
+        next($keMessageArray);
+        $key = key($keMessageArray);
     }
     echo "</table>\n";
 }
@@ -52,7 +89,9 @@ if(!file_exists("messages.txt") || filesize("messages.txt") == 0){
 <p>
 <a href="PostMessage.php">Post New Message </a><br>
 <!-- query string passes data as name value pair -->
-<a href="MessageBoard.php?action=Delete%20First">Delete first Message</a>
+<a href="MessageBoard.php?action=Delete%20First">Delete first Message</a><br>
+<a href="MessageBoard.php?action=Delete%20Last">Delete Last Message</a><br>
+<a href="MessageBoard.php?action=Remove%20Duplicates">Remove duplicates</a><br>
 </p>
 
 </body>
